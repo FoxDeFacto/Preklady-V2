ec9755a2a55e02824d129129273d19e38b516380
Fox De Facto <lisakzezhore@seznam.cz>
2025-02-06 19:42:04 +0100

    cors

--- slovnik-backend/config/middlewares.ts
+++ slovnik-backend/config/middlewares.ts
@@ -1,9 +1,24 @@
-export default [
-  'strapi::logger',
+module.exports = [
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  'strapi::security',  // Added missing required middleware
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://novo-cestina.lynder.dev', 'http://localhost:3000'],
+      headers: [
+        'Content-Type',
+        'Authorization',
+        'Origin',
+        'Accept',
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      keepHeaderOnError: true,
+      credentials: true,
+      maxAge: 86400,
+    },
+  },
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
