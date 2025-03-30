e98bf3e871a41d9698da7b10900d7ef4d13ba8a7
Fox De Facto <lisakzezhore@seznam.cz>
2025-03-30 16:44:39 +0200

    New version of next, new record for strapi

--- slovnik-frontend/src/config.js
+++ slovnik-frontend/src/config.js
@@ -1,2 +1,3 @@
 // config.ts
-export const API = 'https://strapi-administrace.lynder.dev';
+//export const API = 'https://strapi-administrace.lynder.dev';
+export const API ='http://localhost:1337';
