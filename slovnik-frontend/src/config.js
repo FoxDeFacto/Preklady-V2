34b66419b6efc05e3adf6f313276a6e591ec2b5d
Lynder063 <lynder063@noreply.localhost>
2025-04-06 16:49:31 +0000

    Update slovnik-frontend/src/config.js

--- slovnik-frontend/src/config.js
+++ slovnik-frontend/src/config.js
@@ -1,3 +1,4 @@
 // config.ts
-export const API = 'https://strapi-administrace.lynder.dev';
+export const API = 'http://slovnik-backend:1337';
+//export const API = 'https://strapi-administrace.lynder.dev';
 //export const API ='http://localhost:1337';
